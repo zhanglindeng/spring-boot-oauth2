32bfeee1d5042ce1db12539bb0f0b846b2b017c2
zhanglindeng <zhanglindeng@outlook.com>
2018-06-06 11:22:38 +0800

    ClientDetailsService impl 2

--- src/main/java/com/dzlin/oauth2/config/OAuth2AuthorizationServerConfig.java
+++ src/main/java/com/dzlin/oauth2/config/OAuth2AuthorizationServerConfig.java
@@ -1,8 +1,13 @@
 package com.dzlin.oauth2.config;
 
 import com.dzlin.oauth2.config.encryption.ClientPasswordEncoder;
+import com.dzlin.oauth2.entity.OauthClientDetailsEntity;
+import com.dzlin.oauth2.repository.OauthClientDetailsRepository;
+import com.dzlin.oauth2.service.CustomClientDetails;
 import com.dzlin.oauth2.service.CustomClientDetailsService;
 import com.dzlin.oauth2.service.CustomUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +20,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 @Configuration
@@ -30,20 +39,27 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     private DataSource dataSource;
     private AuthenticationManager authenticationManager;
     private CustomUserDetailsService userDetailsService;
+    private OauthClientDetailsRepository oauthClientDetailsRepository;
+    private HttpServletRequest httpServletRequest;
     //    private PasswordEncoder oauthClientPasswordEncoder;
-    private CustomClientDetailsService customClientDetailsService;
+//    private CustomClientDetailsService customClientDetailsService;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public OAuth2AuthorizationServerConfig(DataSource dataSource,
                                            AuthenticationManager authenticationManager,
                                            CustomUserDetailsService userDetailsService,
 //                                           PasswordEncoder oauthClientPasswordEncoder,
-                                           CustomClientDetailsService customClientDetailsService) {
+//                                           CustomClientDetailsService customClientDetailsService,
+                                           OauthClientDetailsRepository oauthClientDetailsRepository,
+                                           HttpServletRequest httpServletRequest) {
         this.dataSource = dataSource;
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
 //        this.oauthClientPasswordEncoder = oauthClientPasswordEncoder;
-        this.customClientDetailsService = customClientDetailsService;
+//        this.customClientDetailsService = customClientDetailsService;
+        this.oauthClientDetailsRepository = oauthClientDetailsRepository;
+        this.httpServletRequest = httpServletRequest;
     }
 
     @Bean
@@ -66,7 +82,7 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(customClientDetailsService);
+        clients.withClientDetails(clientDetailsService());
         // clients.jdbc(dataSource);
     }
 
@@ -75,5 +91,25 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
         endpoints.tokenStore(tokenStore())
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
+    }
+
+    private ClientDetailsService clientDetailsService() {
+        return clientId -> {
+
+            this.logger.info(this.httpServletRequest.getQueryString());
+
+            OauthClientDetailsEntity client = this.oauthClientDetailsRepository.findByClientId(clientId);
+            if (client != null) {
+
+                this.logger.info(client.toString());
+
+                return new CustomClientDetails(client);
+            }
+
+            this.logger.warn("client not found: " + clientId);
+
+            throw new ClientRegistrationException("client not found");
+
+        };
     }
 }
